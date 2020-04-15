335c640e7e2ac95e63a8c1f5c0abcf70f7edaf8e
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-04-15 14:55:40 -0500

    moving out of develop folder

---
+++ models/resources.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, Datatypes){
+    const Resources = sequelize.define("Resources", {
+
+        resource: Datatypes.STRING,
+        description: Datatypes.STRING
+
+    })
+
+    Resources.associate = function(models){
+        Resources.belongsTo(models.Activities, {
+            foreignKey: {
+                allowNull: false,
+            }
+        }) 
+    }
+    return Resources;
+
+
+};
