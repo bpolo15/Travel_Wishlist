6c6eebbe0b8973c03c77b5344858bf83241ac1f3
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-04-18 11:18:35 -0500

    adding new home page

--- db/travelist.sql
+++ db/travelist.sql
@@ -8,7 +8,7 @@ USE travelist_db;
  (`id` INTEGER NOT NULL auto_increment , `email` VARCHAR(255) NOT NULL UNIQUE, `password` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`));
 
  CREATE TABLE IF NOT EXISTS `Destinations` (`id` INTEGER NOT NULL auto_increment , `location` VARCHAR(255), `picture` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, `UserId` INTEGER NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE);
-
+4
  
 CREATE TABLE IF NOT EXISTS `Activities` 
 (`id` INTEGER NOT NULL auto_increment , `activity` VARCHAR(255), `picture` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, `DestinationId` INTEGER NOT NULL, PRIMARY KEY (`id`), 
