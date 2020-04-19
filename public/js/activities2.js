a9d1a14dce32a1525647b3c191f5ea0b15750c18
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-04-19 10:54:11 -0500

    updating data configuration and rendering example card for activities

--- public/js/activities2.js
+++ public/js/activities2.js
@@ -8,29 +8,49 @@ $(document).ready(function() {
       });
 function getDestination(userId){
       $.get('/api/destinations/user/' + userId).then((response) => {
-        console.log("Destinations: ",response)
+        // console.log("Destinations: ",response)
         for(let i = 0; i<response.length; i++) {
           const {id, location} = response[i];
-          console.log(location);
+          console.log(location)
           const dropdown = $(`<a class = "dropdown-item" data-destination-id=${id}>`)
           dropdown.append(location)
-    
           $(".dropdown-menu").append(dropdown)
         }
       })
     }
     $(document).on('click', ".dropdown-item", getIDdest)
     function getIDdest(){
-      console.log("click")
       const id = $(this).attr("data-destination-id")
       console.log(id);
+      $.get('api/destinations/' + id).then((response) => {
+       const destination = response.location;
+        console.log(destination)
+
       renderActivities(id);
+
+      })
+    
+    
     }
 
     function renderActivities(id){
         $.get("/api/activities/destination/" + id).then((response) => {
-            console.log(response);
             $("#newActivity").html('');
+            if(response.length === 0){
+              const card = $('<div class="card">').addClass("card");
+              const image = $('<img class="card-img-top">').attr("src", "https://images.pexels.com/photos/386009/pexels-photo-386009.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500");
+              const cardBody = $("<div>").addClass("card-body");
+              const destinationName = $('<h4 class="card-title text-center">').text(
+                "No activities. Add some to get started!" 
+              );
+              const deleteButton = $(`<span><button class="delete-activity" data-activity-id=${id}>X</button>
+                </span>`);
+              
+              destinationName.append(deleteButton);
+              cardBody.append(image, destinationName);
+              card.append(cardBody);
+              $("#newActivity").append(card);
+            }
             for (let i = 0; i < response.length; i++) {
               const { id, activity, picture } = response[i];
               console.log(id, activity, picture);
